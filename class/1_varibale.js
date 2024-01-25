f901ca053ae65cd92c75ce4a111a3ab3f84a2707
RanajitSahoo <ranajitsahoo170597@gmail.com>
2024-01-26 00:19:26 +0530

    variable in js String datatype

---
+++ class/1_varibale.js
@@ -0,0 +1,43 @@
+/*
+
+ */
+
+let a;//initialization
+a="hey, how are you";//variable declaration
+console.log(a,typeof a);
+a=10;
+let b='a';
+console.log(a,typeof a);
+console.log(b,typeof b);
+
+//creating const
+
+const c=10;
+
+console.log(c);
+
+console.log(c);
+
+
+// console.log(a);
+if(5>2){
+    console.log("in side block");
+    var d=12;
+    let e=13;
+    console.log(a);
+    console.log(d);
+    console.log(e);
+
+}
+// console.log(e) it is giving error
+console.log(d);
+console.log(a);
+
+var x=10;
+var x=20;
+console.log(x);
+console.log(x);
+
+z=24;
+console.log(z);
+var z;
